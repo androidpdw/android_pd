0a1e95565076823a1d8a006c78dacb8f7b2a7d02
Cpz <461354294@qq.com>
2016-04-23 11:36:58 +0800

    添加登录注册页面

--- Project/read/src/main/java/cpz/com/read/activity/MainActivity.java
+++ Project/read/src/main/java/cpz/com/read/activity/MainActivity.java
@@ -1,11 +1,17 @@
 package cpz.com.read.activity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,18 +25,21 @@ import cpz.com.read.view.ChangeColorIconWithTextView;
 
 public class MainActivity extends FragmentActivity implements ViewPager.OnPageChangeListener, View.OnClickListener
 {
-
+    @ViewInject(R.id.iv_user)
+    private ImageView ivUser;
     private ViewPager vpContainer;
     private FragmentPagerAdapter mAdapter;
     private String[] mTitles = new String[]{"1",
             "2", "3", "4"};
     private List<Fragment> mTabs = new ArrayList<Fragment>();
     private List<ChangeColorIconWithTextView> mTabIcon = new ArrayList<ChangeColorIconWithTextView>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewUtils.inject(this);
         initData();
         initUI();
     }
@@ -44,19 +53,23 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
         mAdapter = new FragmentPagerAdapter(getSupportFragmentManager())
         {
             @Override
-            public int getCount() {
+            public int getCount()
+            {
                 return mTabs.size();
             }
 
             @Override
-            public Fragment getItem(int arg0) {
+            public Fragment getItem(int arg0)
+            {
                 return mTabs.get(arg0);
             }
         };
         initTabIndicator();
 
     }
-    private void initTabIndicator() {
+
+    private void initTabIndicator()
+    {
         ChangeColorIconWithTextView one = (ChangeColorIconWithTextView) findViewById(R.id.ctv_home);
         ChangeColorIconWithTextView two = (ChangeColorIconWithTextView) findViewById(R.id.ctv_contact);
         ChangeColorIconWithTextView three = (ChangeColorIconWithTextView) findViewById(R.id.ctv_find);
@@ -67,6 +80,10 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
         mTabIcon.add(three);
         mTabIcon.add(four);
 
+        one.setIconColor(Color.parseColor("#4f5fba"));
+        two.setIconColor(Color.parseColor("#4f5fba"));
+        three.setIconColor(Color.parseColor("#4f5fba"));
+        four.setIconColor(Color.parseColor("#4f5fba"));
 
         one.setOnClickListener(this);
         two.setOnClickListener(this);
@@ -81,12 +98,14 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
         vpContainer = (ViewPager) findViewById(R.id.vp_container);
         vpContainer.setOnPageChangeListener(this);
         vpContainer.setAdapter(mAdapter);
-
+        ivUser.setOnClickListener(this);
     }
 
     @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        if (positionOffset > 0) {
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+    {
+        if (positionOffset > 0)
+        {
             ChangeColorIconWithTextView left = mTabIcon.get(position);
             ChangeColorIconWithTextView right = mTabIcon.get(position + 1);
 
@@ -96,20 +115,24 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
     }
 
     @Override
-    public void onPageSelected(int position) {
+    public void onPageSelected(int position)
+    {
 
     }
 
     @Override
-    public void onPageScrollStateChanged(int state) {
+    public void onPageScrollStateChanged(int state)
+    {
 
     }
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View view)
+    {
         resetOtherTabs();
 
-        switch (view.getId()) {
+        switch (view.getId())
+        {
             case R.id.ctv_home:
                 mTabIcon.get(0).setIconAlpha(1.0f);
                 vpContainer.setCurrentItem(0, false);
@@ -126,11 +149,16 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
                 mTabIcon.get(3).setIconAlpha(1.0f);
                 vpContainer.setCurrentItem(3, false);
                 break;
+            case R.id.iv_user:
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
         }
     }
 
-    private void resetOtherTabs() {
-        for (int i = 0, len = mTabIcon.size(); i < len; i++) {
+    private void resetOtherTabs()
+    {
+        for (int i = 0, len = mTabIcon.size(); i < len; i++)
+        {
             mTabIcon.get(i).setIconAlpha(0);
         }
     }
